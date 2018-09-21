3ea5f2b94083a08536c1788e66ed1c580e86acb0
jairoHer <32321394+jairoHer@users.noreply.github.com>
2018-09-21 04:18:19 -0600

    se especifico la base de datos a usar en la configuracion

--- E-commerce/config/db.php
+++ E-commerce/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=practicaAYD1',
     'username' => 'root',
-    'password' => '',
+    'password' => '',/*Pongan la clave que tengan a su mySql*/
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
