40c1f80066a0e346da520b19e01e42faebdb34c7
kevin <kevinalbertoorellana@gmail.com>
2018-09-22 23:29:42 -0600

    201403762 - Correccion de pruebas para asegurar el code coverage

--- E-commerce/tests/unit/utilities/OperacionesCategoriaTest.php
+++ E-commerce/tests/unit/utilities/OperacionesCategoriaTest.php
@@ -3,15 +3,31 @@ namespace tests\utilities;
 use app\utilities\OperacionesCategoria;
 class OperacionesCategoriaTest extends \Codeception\Test\Unit
 {
-  private $codigo_categoria = 3;//Arrange
+  private $codigo_categoria = 1000;//Arrange
   private $nombre_categoria = "nombre";//Arrange
+
   public function testNuevaCategoria(){
-    $result =OperacionesCategoria::Nueva_Categoria($this->$nombre_categoria);//Act
+    $result =OperacionesCategoria::Nueva_Categoria("nombre", 1000);//Act
     $this->assertTrue($result);//Assert
   }
+
+  public function testNuevaCategoriaSinCodigo(){
+    $result = OperacionesCategoria::Nueva_Categoria("nombre",null);//Act
+    $this->assertFalse($result);//Assert
+  }
+
+
+   /**
+   * @depends testNuevaCategoria
+   */
   public function testQuitarCategoria(){
-    $result =OperacionesCategoria::Quitar_Categoria($this->$codigo_categoria);//Act
+    $result = OperacionesCategoria::Quitar_Categoria(3);//Act
     $this->assertTrue($result);//Assert
+  }
+
+  public function testQuitarCategoriaQueNoExiste(){
+    $result = OperacionesCategoria::Quitar_Categoria(3000);//Act
+    $this->assertFalse($result);//Assert
   }
   public function testObtenerListaDeCategoriasDisponibles(){
     $result = OperacionesCategoria::get_categorias_disponibles();//Act
--- E-commerce/utilities/OperacionesCategoria.php
+++ E-commerce/utilities/OperacionesCategoria.php
@@ -8,37 +8,27 @@ use app\models\Producto;
 
 class OperacionesCategoria{
   public static function Nueva_Categoria($nombre_categoria, $codigo){
-    try{
-          $categoria = new Categoria;
-          $categoria->nombre = $nombre_categoria;
-          $categoria->codigo = $codigo;
-          $categoria->save();
-          return true;
-    } catch(Exception $e){
-
-    } // catch
+      if($nombre_categoria!=null&&$codigo!=null){
+        $categoria = new Categoria;
+        $categoria->nombre = $nombre_categoria;
+        $categoria->codigo = $codigo;
+        $categoria->save();
+        return true;
+      }
       return false;
   }
-  public static function Quitar_Categoria($codigo_producto){
-    try{
-      $producto_row = Categoria::find()->where('codigo = :codigo', [':codigo' => $codigo_producto])->one();
-      $producto_row->delete();
-      return true;
-    } catch(Exception $e){
 
-    } // catch
+  public static function Quitar_Categoria($codigo_producto){
+      $producto_row = Categoria::find()->where('codigo = :codigo', [':codigo' => $codigo_producto])->one();
+      if($producto_row!=null){
+        $producto_row->delete();
+        return true;
+      }
       return false;
   }
 
   public static function get_categorias_disponibles(){
       $query_string= "SELECT * from categoria";
-      $connection = Yii::$app->getDb();
-      $command = $connection->createCommand($query_string);
-      $result = $command->queryAll();
-      return $result;
-  }
-  public static function get_productos_comprados(){
-      $query_string= "SELECT * from Producto where estado = 2;";
       $connection = Yii::$app->getDb();
       $command = $connection->createCommand($query_string);
       $result = $command->queryAll();
